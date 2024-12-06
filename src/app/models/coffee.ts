c0e9393fb8c68e8d45b838c78480e7ea4fa3a238
Sophie Théréau <sophiethereau@hotmail.fr>
2024-12-06 12:32:18 +0100

    chore: add coffee service

--- src/app/models/coffee.ts
+++ src/app/models/coffee.ts
@@ -7,15 +7,17 @@ export class Coffee {
         allergen : boolean
     }];
     price: number;
+    shops_id: [];
     
     constructor(id: string, name: string, description:string, ingredients:[{
         name: string,
         allergen : boolean
-    }], price: number) {
+    }], price: number, shops_id:[]) {
       this.id = id;
       this.name = name;
       this.description = description;
       this.ingredients = ingredients;
       this.price = price;
+      this.shops_id = shops_id;
     }
   }
---
+++ src/app/services/coffee/coffee.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Coffee } from '../../models/coffee';
+import { environment } from '../../environment/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CoffeeService {
+
+  constructor(private httpClient:HttpClient) { }
+
+  apiUrl: string = environment.apiUrl+"coffees";
+
+  getCoffees(): Observable<Coffee[]>{
+    return this.httpClient.get<Coffee[]>(this.apiUrl);
+  }
+
+  getCoffee(id : string){
+    return this.httpClient.get(this.apiUrl+"/"+id);
+  }
+
+  createCoffee(coffee: Coffee){
+    return this.httpClient.post(this.apiUrl, coffee);
+  }
+
+  updateCoffee(coffee: Coffee, id : string){
+    return this.httpClient.put(this.apiUrl+"/"+id, coffee);
+  }
+
+  deleteCoffee(id : string){
+    return this.httpClient.delete(this.apiUrl+"/"+id);
+  }
+}
