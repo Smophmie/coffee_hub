23c6d674b9a16f483c094f8c8eff55bc8e08ee1b
Sophie Théréau <sophiethereau@hotmail.fr>
2024-12-09 10:25:17 +0100

    chore: checkboxes for shops

--- src/app/components/addcoffeeform/addcoffeeform.component.ts
+++ src/app/components/addcoffeeform/addcoffeeform.component.ts
@@ -1,31 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 import { CoffeeService } from '../../services/coffee/coffee.service';
 import { Router } from '@angular/router';
+import { Shop } from '../../models/shop';
+import { ShopService } from '../../services/shop/shop.service';
+import { NgIf, NgForOf } from '@angular/common';
 
 
 @Component({
   selector: 'app-addcoffeeform',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgIf, NgForOf],
   templateUrl: './addcoffeeform.component.html',
   styleUrl: './addcoffeeform.component.css'
 })
 
-export class AddcoffeeformComponent {
+export class AddcoffeeformComponent implements OnInit {
 
   constructor(
     private coffeeService: CoffeeService,
+    private shopService: ShopService,
     private router: Router,
   ){};
+
+  shops: Shop[] = [];
 
   creation_form: FormGroup = new FormGroup({
     name: new FormControl<string>(''),
     description: new FormControl<string>(''),
-    price: new FormControl<number|null>(null)
+    price: new FormControl<number|null>(null),
+    shops_id: new FormControl([])
   });
+
+  ngOnInit(){
+    // Charger toutes les boutiques pour créer les checkboxes
+    this.shopService.getShops().subscribe({
+      next: (data: Shop[]) => {
+        this.shops = data;
+        console.log(this.shops);        
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    });
+  }
 
   onSubmit() {
     const formData = this.creation_form.value;
+    console.log(formData);
     this.coffeeService.createCoffee(formData).subscribe({
       next: (response) => {
         console.log('Coffee created successfully:', response);
