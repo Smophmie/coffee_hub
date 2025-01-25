dda1f19bc5adebc89ba42d30ef2d15736b57ac3e
Sophie Théréau <sophiethereau@hotmail.fr>
2025-01-25 19:24:13 +0100

    chore: add list of shops where each coffee is sold

--- src/app/models/coffee.ts
+++ src/app/models/coffee.ts
@@ -7,17 +7,23 @@ export class Coffee {
         allergen : boolean
     }];
     price: number;
-    shops_id: [];
+    shops_id: string [];
+    shop_names?: string[];
     
     constructor(id: string, name: string, description:string, ingredients:[{
         name: string,
         allergen : boolean
-    }], price: number, shops_id:[]) {
+      }], 
+      price: number, 
+      shops_id: string [],
+      shop_names?: string[]
+    ) {
       this.id = id;
       this.name = name;
       this.description = description;
       this.ingredients = ingredients;
       this.price = price;
-      this.shops_id = shops_id;
+      this.shops_id = shops_id,
+      this.shop_names = shop_names
     }
-  }
+}
